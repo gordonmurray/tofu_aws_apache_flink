ce46098776e453c44dc1904f7cb61e4823a42573
Gordon Murray <gordon@gordonmurray.com>
2024-02-17 14:52:13 +0000

    output flink job manager IP

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "flink_job_managers_public_ip" {
-  value = aws_instance.flink_job_managers.public_ip
+  value       = "http://${aws_instance.flink_job_managers[0].public_ip}:8081"
   description = "Public IP address of the Flink job managers instance."
 }
