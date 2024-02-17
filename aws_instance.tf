20b94f697519547fe3c9f8b70dc672d095fd432a
Gordon Murray <gordon@gordonmurray.com>
2024-02-17 12:10:10 +0000

    user data to interpolate the ip address

--- aws_instance.tf
+++ aws_instance.tf
@@ -3,11 +3,14 @@ resource "aws_instance" "flink_job_managers" {
   instance_type           = var.flink_jobmanager_instance_type
   count                   = var.flink_jobmanager_instance_count
   key_name                = aws_key_pair.flink.key_name
-  vpc_security_group_ids  = [aws_security_group.flink.id]
+  vpc_security_group_ids  = [aws_security_group.flink_security_group.id]
   disable_api_termination = true
   subnet_id               = element(var.subnets, count.index)
   iam_instance_profile    = aws_iam_instance_profile.ec2_s3_write_profile.id
-  tags                    = var.tags
+  tags = {
+    Name   = "Apache Flink v1.17.2 Job Manager"
+    Source = "https://github.com/gordonmurray/tofu_aws_apache_flink"
+  }
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
@@ -15,7 +18,13 @@ resource "aws_instance" "flink_job_managers" {
     # Wait for the system to start up fully
     sleep 30
 
-    /home/ubuntu/flink/bin/jobmanager.sh start
+    # Get the IP address of the instance
+    TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+    PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+    sed -i "s/ACTUAL_IP_ADDRESS/$PUBLIC_IP/g" /home/ubuntu/flink/conf/flink-conf.yaml
+
+    # start the flink job manager as the ubuntu user
+    su - ubuntu -c '/home/ubuntu/flink/bin/start-cluster.sh'
     EOF
   )
 
